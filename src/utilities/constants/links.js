844141ead143eb3a691fc0fa11e2f7162965afb5
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-03-30 19:43:09 +0600

    implement navlinks object in link.js file

---
+++ src/utilities/constants/links.js
@@ -0,0 +1,15 @@
+const navLinks = {
+    main: [
+        { id: 1, name: "Products", path: "/products" },
+        { id: 2, name: "Features", path: "/features" },
+        { id: 3, name: "Pricing", path: "/pricing" },
+        { id: 4, name: "Testimonials", path: "/testimonials" },
+        { id: 5, name: "FAQ", path: "/faq" }
+    ],
+    auth: [
+        { id: 6, name: "Login", path: "/login" },
+        { id: 7, name: "Get Started", path: "/get-started" }
+    ]
+};
+
+export default navLinks;
