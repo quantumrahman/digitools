4750c51b5293fe8a209c3f23d68177c1fa1607ae
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-03-30 23:41:42 +0600

    rename Body to Main folder and add Main.jsx component in Layout.jsx component

--- src/components/layouts/Layout/Layout.jsx
+++ src/components/layouts/Layout/Layout.jsx
@@ -1,11 +1,12 @@
 import Header from "../Header/Header.jsx";
+import Main from "../Main/Main.jsx";
 
 const Layout = () => {
     return (
         <>  
             <div className="w-full min-h-screen relative overflow-hidden">
                 <Header></Header>
-                
+                <Main></Main>
             </div>
         </>
     );
