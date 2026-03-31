3e94ed366e93072ad129ac5a3e02a0284369129e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-03-31 11:04:22 +0600

    add StatsSection component in the Main.jsx file

--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,9 +1,11 @@
 import HomeSection from '../../ui/HomeSection/HomeSection.jsx';
+import StatsSection from '../../ui/StatsSection/StatsSection.jsx';
 
 const Main = () => {
     return (
         <>
             <HomeSection></HomeSection>
+            <StatsSection></StatsSection>
         </>
     );
 };
