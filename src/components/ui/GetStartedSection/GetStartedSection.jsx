ed2576b17c51981f83250e57b41a5f2d4d345024
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-03-31 17:13:00 +0600

    implement heading section text in GetStatedSection

--- src/components/ui/GetStartedSection/GetStartedSection.jsx
+++ src/components/ui/GetStartedSection/GetStartedSection.jsx
@@ -1,7 +1,17 @@
 const GetStartedSection = () => {
     return (
         <>
-        
+            <div className='w-full py-30 bg-[#F9FAFC]'>
+                <div className='max-w-300 mx-auto px-4 sm:px-6 lg:px-8'>
+                    <div className='w-full text-center space-y-4'>
+                        <h2 className='manrope text-5xl font-extrabold text-[#101727]'>Get Started in 3 Steps</h2>
+                        <p className='manrope text-base font-normal text-[#627382]'>Start using premium digital tools in minutes, not hours.</p>
+                    </div>
+                    <div className='w-full mt-10 grid grid-cols-3 gap-7.5'>
+                        
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
